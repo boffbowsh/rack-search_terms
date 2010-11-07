f2d99100907d5a6b0302f166c7c85a5dfee99a97
Paul Bowsher <paul.bowsher@gmail.com>
2010-11-07 22:20:29 +0000

    Welp, that's Google working

--- lib/rack-search_terms.rb
+++ lib/rack-search_terms.rb
@@ -1,19 +1,43 @@
+require 'rack/utils'
+
 module Rack
   class SearchTerms
+    include Rack::Utils
+    
     def initialize app
       @app = app
     end
     
     def call env
-      env.merge! 'search_engine' => search_engine if search_engine
-      env.merge! 'search_terms' => search_terms if search_terms
-      @app.call env
+      @env = env
+      @env.merge! 'search_engine' => search_engine if search_engine
+      @env.merge! 'search_terms' => search_terms if search_terms
+      @app.call @env
+    end
+    
+    def referer
+      @env['HTTP_REFERER']
     end
     
     def search_engine
+      @search_engine ||= case referer 
+      when /http:\/\/www.google.com\/\?q=/
+        'Google Web'
+      end
     end
     
     def search_terms
+      case referer 
+      when /^http:\/\/www\.google\..*?\/imgres/
+        prev = terms_from_param 'prev'
+        parse_query(prev.split('?')[1])['q']
+      when /^http:\/\/www\.google\..*?\//
+        terms_from_param 'q'
+      end
+    end
+    
+    def terms_from_param param
+      parse_query(URI.parse(referer).query)[param]
     end
     
     def self.version; '0.1.0'; end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,20 @@ require 'rspec'
 Rspec.configure do |c|
   c.mock_with :rspec
 end
+
+def expect_env hash={}
+  @app.should_receive(:call).with( hash_including(hash) )
+end
+
+def mock_request referer
+  middleware = Rack::SearchTerms.new(@app)
+  yield middleware if block_given?
+  middleware.call({'HTTP_REFERER' => referer})
+end
+
+def it_should_work_for name, options
+  it "should work for #{name}" do
+    expect_env 'search_terms' => options[:as]
+    mock_request options[:from]
+  end
+end
